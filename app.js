4b7ab15fc452d2d670deba2051012241277fc333
anupriya645@gmail.com <guptaanupriya645@gmail.com>
2024-11-28 01:42:30 +0530

    save changes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 const mongo_url="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -32,6 +34,18 @@ app.get("/",(req,res)=>{
     res.send("Hi , i am root");
 });
 
+const validateListing = (req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    // console.log(result);
+    if(error) {
+        let errMsg = error.details.map( (el)=>el.message).join(",");
+        throw new ExpressError(400,error);
+    }
+    else{
+        next();
+    }
+}
+
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing=new Listing({
 //         title:"My new villa", 
@@ -48,11 +62,12 @@ app.get("/",(req,res)=>{
 
 
 // INDEX ROUTE
-app.get("/listings",async (req,res)=>{
+app.get("/listings",  wrapAsync(async (req,res)=>{
  const allListings= await Listing.find({});
 //  res.render("/listings/index.ejs", {allListings});
 res.render("listings/index.ejs", {allListings});
-});
+})
+);
 
 // NEW ROUTE 
 app.get("/listings/new",(req,res)=>{
@@ -60,45 +75,102 @@ app.get("/listings/new",(req,res)=>{
  });
 
 // SHOW ROUTE
-app.get("/listings/:id", async (req,res)=>{
-   let { id }=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
-});
+// app.get("/listings/:id", async (req,res)=>{
+//    let { id }=req.params;
+//     const listing=await Listing.findById(id);
+//     res.render("listings/show.ejs",{listing});
+// });
+
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+    try{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+        return res.status(404).send("Listing not found");
+        }
+    res.render("listings/show.ejs", { listing });
+    }
+    catch(err){
+        res.status(404).send("listing not found");
+    }
+}));
 
 // CREATE ROUTE
-app.post("/listings", wrapAsync( async (req,res,next)=>{
+app.post("/listings",validateListing, wrapAsync( async (req,res,next)=>{
+    // let result=listingSchema.validate(req.body);
+    // console.log(result);
+    // if(result.error) {
+    //     throw new ExpressError(400,result.error)
+    // }
+    // if(!req.body.listing){
+    //    throw new ExpressError(400,"Send valid data for listing"); 
+    // }
   const newListing=new Listing(req.body.listing);
+//   if(!newListing.title){
+//     throw new ExpressError(400,"title is missing"); 
+//   }
+//   if(!newListing.description){
+//     throw new ExpressError(400,"description is missing"); 
+//   }
+//   if(!newListing.location){
+//     throw new ExpressError(400,"location is missing"); 
+//   }
+
   await newListing.save();
   res.redirect("/listings");
  })
 );
 
 // EDIT ROUTE
-app.get("/listings/:id/edit", async(req,res)=>{
+app.get("/listings/:id/edit",  wrapAsync(async(req,res)=>{
     let { id }=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+}));
 
 // UPDATE ROUTE
-app.put("/listings/:id", async(req,res)=>{
+app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
+    if (!listing) {
+        return res.status(404).send("Listing not found");
+        }
     let { id }=req.params;
     await Listing.findByIdAndUpdate(id,{ ...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
+
+// app.delete("/listings/:id",  wrapAsync(async (req, res) => {
+//     try {
+//         let { id } = req.params;
+//         let deletedlisting = await Listing.findByIdAndDelete(id);
+//         if(!deletedlisting){
+//             console.log("lisitng is not available");
+//         }
+//         console.log(deletedlisting);
+//         res.redirect("/listings")
+//     } catch (err) {
+//         res.status(404).send("listing not found");
+//     }
+// }));
 
 // DELETE ROUTE
-app.delete("/listings/:id", async(req,res)=>{
+app.delete("/listings/:id", wrapAsync(async(req,res)=>{
     let { id }=req.params;
    let deletedlisting=await Listing.findByIdAndDelete(id);
    console.log(deletedlisting);
    res.redirect("/listings")
+}));
+
+app.all("*",(req,res,next)=>{
+   next(new ExpressError(404,"Page Not Found")) ;
 });
 
 
 app.use( (err,req,res,next)=>{
-res.send("Something went wrong");
+    let { statuscode=500, message="Something went wrong"} = err;
+    res.status(statuscode).render("error.ejs",{message});
+    // res.status(statuscode).send(message);
+
+// res.send("Something went wrong");
 });
 
 app.listen(8080,()=>{

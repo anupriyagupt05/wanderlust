9de57c05ac8193fe61650eab7cf89007b19f3682
anupriya645@gmail.com <guptaanupriya645@gmail.com>
2024-12-04 01:20:09 +0530

    save changes

--- app.js
+++ app.js
@@ -168,17 +168,29 @@ app.all("*",(req,res,next)=>{
 
 // REVIEWS
 // POST ROUTE
+// app.post("/listings/:id/reviews", async(req,res)=>{
+//   let listing = await Listing.findById(req.params.id);
+//   let newReview = new Review(req.body.review);
+
+//   listing.reviews.push(newReview);
+//   await newReview.save();
+//   await listing.save();
+
+//   console.log("new review saved");
+//   res.redirect("new review saved");
+// });
+
 app.post("/listings/:id/reviews", async(req,res)=>{
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
-
-  listing.reviews.push(newReview);
-  await newReview.save();
-  await listing.save();
-
-  console.log("new review saved");
-  res.send("new review saved");
-});
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+  
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+  
+    console.log("new review saved");
+      res.redirect(`/listings/${listing._id}`);
+  });
 
 
 
